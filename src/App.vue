b830141294fc72eab4379951f9497e86a248ad7a
Nick <nicknbt25@gmail.com>
2021-12-04 13:53:52 -0500

    tab indents

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,3 @@ export default {
     },
 };
 </script>
-
-<style lang="scss">
-#app {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
